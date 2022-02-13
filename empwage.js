5485aaadc0fec02bef2cbdd63cb71f507ed556c6
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-02-13 23:41:52 +0530

    added uc10

--- empwage.js
+++ empwage.js
@@ -269,3 +269,25 @@ b. Show the full workings days, part working days and no working days
 
 
 {
+    // UC10 : Ability to store the Day, Hours Worked and Wage Earned in a single object.
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    let empDailyHrsAndWageArr = new Array();
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        let empHrs = getWorkingHours(empCheck);
+        totalEmpHrs += empHrs;
+        empDailyHrsAndWageArr.push(
+            {
+                dayNum: totalWorkingDays,
+                dailyHours: empHrs,
+                dailyWage: calcDailyWage(empHrs),
+                toString() {
+                    return "\nDay" + this.dayNum + " => Working Hours is " + this.dailyHours +
+                        " And Wage Earned = " + this.dailyWage
+                },
+            });
+    }
+    console.log("\nUC10 Showing Daily Hours Worked and Wage Earned: " + empDailyHrsAndWageArr);
+
