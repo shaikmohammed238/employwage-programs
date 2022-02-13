e60a3df4589f42ba4449b151690ee6c7234c423a
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-02-13 23:30:02 +0530

    added uc3

--- empwage.js
+++ empwage.js
@@ -35,6 +35,22 @@ const WAGE_PER_HOUR = 20;
 }
 
 
-
-
+// UC 3: Refactored code to write function for daily working Hours
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+{
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = getWorkingHours(empCheck);
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("UC3 - Emp Wage: " + empWage, " Total Working Hours: " + empHrs);
+}
 
