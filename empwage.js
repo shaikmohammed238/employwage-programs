85e6e04d61b0c2245f84b3816981754ebde95116
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-02-13 23:33:18 +0530

    added uc5

--- empwage.js
+++ empwage.js
@@ -68,3 +68,21 @@ function getWorkingHours(empCheck) {
 }
 
 
+/* UC 5: Calculate Wages till a condition of total working hours of 160 or
+         max days of 20 is reached for a month */
+{
+    const MAX_HRS_IN_MONTH = 160;
+    const NUM_OF_WORKING_DAYS = 20;
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+        totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("UC5 - Total Days: " + totalWorkingDays +
+        ", Total Hrs: " + totalEmpHrs + ", Emp Wage: " + empWage);
+}
+
